0d5171dda748e1943a72a4244a56c0e4b976515b
Fuad Aliah <eye.design17@yahoo.com>
2020-03-21 20:03:16 +0200

    Add files via upload

--- js/locales_ar.js
+++ js/locales_ar.js
@@ -1,6 +1,7 @@
 var locales_ar = {
     "home": "الوطن",
     "news_events": "اخبار وأحداث",
-    "About_us": "حولنا"
-
+    "About_us": "حولنا",
+    "gallery": "انا",
+    "Our_partners": "رثث"
 }
--- js/locales_en.js
+++ js/locales_en.js
@@ -1,6 +1,10 @@
 var locales_en = {
     "home": "home",
     "news_events": "News / Events",
-    "About_us": "About us"
-
+    "About_us": "About us",
+    "gallery": "Our Gallery",
+    "Our_partners": "Our Partners",
+    "Our_library": "Our Library",
+    "Registration": "Registration",
+    "Contact_us": "Contact us"
 }
