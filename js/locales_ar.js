47e979ef1b0977b74d4a13a0552e8213db4d1d31
FuadAliah <eye.design17@yahoo.com>
2020-03-23 01:20:13 +0200

    Arabic updates

--- js/locales_ar.js
+++ js/locales_ar.js
@@ -1,10 +1,39 @@
 var locales_ar = {
-    "home": "الوطن",
+    "home": "الرئيسية",
     "news_events": "الأخبار والفعاليات",
     "About_us": "من نحن",
     "gallery": "معرض الصور",
     "Our_partners": "الشركاء",
     "Our_library": "المكتبة",
+
+    //home_section
     "registration": "سجل فكرتك الآن",
-    "Contact_us": "تواصل معنا"
+    "Contact_us": "تواصل معنا",
+    "about_street": "ما هو برنامج حاضنة الأعمال لرياضات الشوارع؟",
+    "register_now": "سجل فكرتك الآن",
+    "implement": "بتنفيذ من",
+    "facebook": "فيسبوك",
+    "twitter": "تويتر",
+    "youtube": "يوتيوب",
+    "linkedin": "لينكد إن",
+    "instagram": "إنستغرام",
+
+    //news_section
+    "read_more": "إقرأ المزيد",
+    "event": "فعاليات",
+    "events": "فعاليات",
+    "news": "أخبار",
+    "discoverAll": "استكشف الجميع",
+
+    //about_section
+    "watchVideo": "شاهد الفيديو",
+
+    //gallery_section
+    "seeAll": "مشاهدة الجميع",
+
+    //library_section
+    "seeList": "مشاهدة الجميع",
+
+    //subscribe_section
+    "subscribe": "إشترك",
 }
--- js/locales_en.js
+++ js/locales_en.js
@@ -5,6 +5,35 @@ var locales_en = {
     "gallery": "Our Gallery",
     "Our_partners": "Our Partners",
     "Our_library": "Our Library",
+
+    //home_section
     "registration": "Registration",
-    "Contact_us": "Contact us"
+    "Contact_us": "Contact us",
+    "about_street": "WHAT IS STREET SPORT INCUBATOR?",
+    "register_now": "Register Now",
+    "implement": "Project by",
+    "facebook": "Facebook",
+    "twitter": "Twitter",
+    "youtube": "Youtube",
+    "linkedin": "Linkedin",
+    "instagram": "Instagram",
+
+    //news_section
+    "read_more": "Read More",
+    "event": "event",
+    "events": "events",
+    "news": "news",
+    "discoverAll": "Discover All",
+
+    //about_section
+    "watchVideo": "Watch our video",
+    
+    //gallery_section
+    "seeAll": "See All",
+
+    //library_section
+    "seeList": "See List",
+
+    //subscribe_section
+    "subscribe": "subscribe"
 }
